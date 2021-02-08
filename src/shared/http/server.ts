4c47d4f9a8093c9ceb98029595040372797cdeee
LUCIANO ARAUJO MENESES - GETIC <214083@int.cagece.com.br>
2021-02-08 14:15:48 -0300

    Criação da conexão com o banco de dados postgres

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,29 @@
-console.log('fdf');
+import 'reflect-metadata';
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import routes from './routes';
+import AppError from '@shared/errors/appError';
+import '@shared/typeorm';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+  return res.status(500).json({
+    status: 'error',
+    message: 'internal server error',
+  });
+});
+
+app.listen(3333, () => {
+  console.log('server started on port 3333');
+});
